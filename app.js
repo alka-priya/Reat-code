f135e3c3d4a2fc9ac574cec00b60d34231ce43c8
alka-priya <rozy2alka@gmail.com>
2025-07-19 09:23:36 +0530

    commit first changes

---
+++ app.js
@@ -0,0 +1,14 @@
+const heading = React.createElement('div', {id: 'parent', className: 'color'}, [
+    React.createElement('div', {id: 'child1'}, React.createElement('h1', {id:'heading'}, 'Alka priya')),
+    React.createElement('div', {id: 'child2'}, 'I am child 2')]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
+
+
+/**
+ * Ex
+ * <div>
+ *  <div class="child1">I am child 1</div>
+ *  <div class="child1">I am child 1</div>
+ * </div>
+ */
