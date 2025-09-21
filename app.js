95ed8e0837ae68d34d667311c2bac5d5c068ba74
alka-priya <rozy2alka@gmail.com>
2025-09-21 17:55:21 +0530

    changes in code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const heading = React.createElement('div', {id: 'parent', className: 'color'}, [
     React.createElement('div', {id: 'child2'}, 'I am child 2')]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading)
+root.render(Headers);
 
 
 /**
